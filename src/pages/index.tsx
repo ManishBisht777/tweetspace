793c75cbaffc600a8bf5ed8f161b99d8001ecbe2
manish bisht <manish205770@keshav.du.ac.in>
2023-03-03 13:37:10 +0530

    feat: testimonials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Features from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
+import Testimonial from "@/components/home/Testimonials";
 import Navbar from "@/components/navbar/Navbar";
 import Head from "@/components/utilities/Head";
 import useArea from "@/hooks/useArea";
+import Container from "@/layouts/Container";
 
 export default function Home() {
   const { width } = useArea();
@@ -14,6 +16,9 @@ export default function Home() {
         {width >= 1024 ? <Navbar /> : <Navbar isMobile />}
         <Hero />
         <Features />
+        <Container>
+          <Testimonial />
+        </Container>
       </section>
     </>
   );
