16343d9c34f6ecbb5a020e191548786e46f1d091
manish bisht <manish205770@keshav.du.ac.in>
2023-03-08 20:15:33 +0530

    feat: profile details section

--- src/pages/user/[userId]/index.tsx
+++ src/pages/user/[userId]/index.tsx
@@ -1,13 +1,43 @@
+import Container from "@/layouts/Container";
 import React from "react";
 
 type Props = {};
 
 const index = (props: Props) => {
   return (
-    <div>
-      user
-      <p>profile page</p>
-    </div>
+    <Container>
+      <div
+        className="flex justify-center items-center flex-col md:gap-2
+      "
+      >
+        {/* replace with image later */}
+        <div className="w-20 h-20 rounded-full bg-gray-500 md:w-40 md:h-40"></div>
+        <h3 className="text-lg font-semibold text-accent-base md:text-xl">
+          Manish Bisht
+        </h3>
+        <p className="text-sm text-skin-base/80 text-center md:text-lg">
+          Lorem, ipsum dolor sit amet consectetur adipisicing.
+        </p>
+
+        <div className="flex gap-2 text-xs flex-wrap justify-center mt-3">
+          <p className="md:text-[1rem] md:py-2 px-4 py-1 bg-accent-inverted/30 rounded-sm text-accent-inverted font-medium">
+            HTML
+          </p>
+          <p className="md:text-[1rem] md:py-2 px-4 py-1 bg-accent-inverted/30 rounded-sm text-accent-inverted font-medium">
+            HTML
+          </p>
+          <p className="md:text-[1rem] md:py-2 px-4 py-1 bg-accent-inverted/30 rounded-sm text-accent-inverted font-medium">
+            HTML
+          </p>
+          <p className="md:text-[1rem] md:py-2 px-4 py-1 bg-accent-inverted/30 rounded-sm text-accent-inverted font-medium">
+            HTML
+          </p>
+          <p className="md:text-[1rem] md:py-2 px-4 py-1 bg-accent-inverted/30 rounded-sm text-accent-inverted font-medium">
+            HTML
+          </p>
+        </div>
+      </div>
+    </Container>
   );
 };
 
