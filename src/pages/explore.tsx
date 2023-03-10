98a0205853ad7ef77bc876acb091c66600a1aa65
manish bisht <manish205770@keshav.du.ac.in>
2023-03-10 17:26:16 +0530

    feat: explore page

---
+++ src/pages/explore.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const explore = (props: Props) => {
+  return (
+    <div>
+      explore Page
+      <h1>explore</h1>
+    </div>
+  );
+};
+
+export default explore;
