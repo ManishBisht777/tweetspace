6f3722dd0b8ac376f2050be3ef6d4d27024d2407
manish bisht <manish205770@keshav.du.ac.in>
2023-03-11 13:20:04 +0530

    feat: zustand setup and initial value

---
+++ src/store/store.ts
@@ -0,0 +1,25 @@
+import { Session } from "next-auth";
+import { create } from "zustand";
+
+interface userSession extends Session {
+  id: string;
+}
+
+type StoreValues = {
+  session: userSession;
+};
+
+const useStore = create<StoreValues>((set, get) => ({
+  session: {
+    id: "",
+    expires: "",
+  },
+
+  setSession: (session: userSession) => {
+    set({
+      session: session,
+    });
+  },
+}));
+
+export default useStore;
