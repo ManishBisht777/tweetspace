e52a1a755815be293b36079472bc3813f16fd26b
manish bisht <manish205770@keshav.du.ac.in>
2023-03-12 23:24:05 +0530

    feat: hero section dark

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -10,20 +10,20 @@ const Hero = (props: Props) => {
         One stop to Spaces and events
       </p>
 
-      <h1 className="md:text-4xl text-xl font-Montserrat font-bold text-skin-base md:max-w-[30rem] max-w-[18rem] text-center">
+      <h1 className="md:text-4xl text-xl font-Montserrat font-bold text-skin-base dark:text-skin-inverted/90 md:max-w-[30rem] max-w-[18rem] text-center">
         Work on big Ideas, without the{" "}
         <span className="p-[.1rem] text-skin-inverted font-extrabold relative before:absolute before:bg-accent-base before:inset-0 before:z-[-1] before:rotate-[4deg] before:translate-y-1">
           Busywork
         </span>
       </h1>
-      <p className="max-w-[14rem] md:max-w-[23rem] text-skin-base/80 text-center md:text-lg  text-sm">
+      <p className="max-w-[14rem] md:max-w-[23rem] text-skin-base/80 dark:text-skin-inverted/60 text-center md:text-lg  text-sm">
         Contrary to popular belief, Lorem Ipsum is not simply random text. It
         has roots.
       </p>
 
       <div className="flex gap-4">
         <button className="text-sm md:px-6 md:py-2 px-3 py-[.1rem] bg-skin-inverted/95 text-skin-inverted rounded-full">
-          Explore
+          Github
         </button>
         <button className="text-sm md:px-6 md:py-2 px-3 py-[.1rem] bg-skin-inverted/95 text-skin-inverted rounded-full">
           Explore
