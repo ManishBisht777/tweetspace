9965715b10047e83e585473862fb0846c62d5464
manish bisht <manish205770@keshav.du.ac.in>
2023-04-11 16:14:38 +0530

    chores: style changes

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -8,19 +8,19 @@ function Filter({ setTimeFilter }: Props) {
   return (
     <div className="flex mt-5 gap-4">
       <button
-        className="px-4 py-1 bg-accent-inverted/10 rounded-full"
+        className="px-4 py-1 bg-accent-inverted/10 rounded-full border border-accent-inverted text-accent-inverted"
         onClick={() => setTimeFilter("past")}
       >
         Past
       </button>
       <button
-        className="px-4 py-1 bg-accent-base/10 rounded-full"
+        className="px-4 py-1 bg-accent-base/10 rounded-full border border-accent-base text-accent-base"
         onClick={() => setTimeFilter("ongoing")}
       >
         Ongoing
       </button>
       <button
-        className="px-4 py-1 bg-accent-muted/10 rounded-full"
+        className="px-4 py-1 bg-accent-muted/10 rounded-full border border-accent-muted text-accent-muted"
         onClick={() => setTimeFilter("future")}
       >
         Future
